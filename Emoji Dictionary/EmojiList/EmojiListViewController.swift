a518758069bf182df315c55887067b87beb4d92b
Scott Moon <jiwonsis@naver.com>
2017-12-17 00:01:26 +0900

    기본 테이블 뷰 프로토콜 설정

--- Emoji Dictionary/EmojiList/EmojiListViewController.swift
+++ Emoji Dictionary/EmojiList/EmojiListViewController.swift
@@ -18,3 +18,18 @@ class EmojiListViewController: UIViewController {
         title = "Emoji List"
     }
 }
+
+extension EmojiListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
