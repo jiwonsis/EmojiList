014e5b3cf76a17a3aa4e1ab13acad54a8f31d946
Scott Moon <jiwonsis@naver.com>
2017-12-16 23:58:10 +0900

    뷰 컨트롤러 생성

---
+++ Emoji Dictionary/EmojiList/EmojiListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiListViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Scott moon on 16/12/2017.
+//  Copyright © 2017 Scott moon. All rights reserved.
+//
+
+import UIKit
+
+class EmojiListViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Emoji List"
+    }
+}
